cac39df978dda09799e7957a2dc6cb25b2b03d71
paw.frolow <paw.frolow@gmail.com>
2024-02-03 19:16:21 +0300

    added another facts

---
+++ src/data/facts/index.ts
@@ -0,0 +1,12 @@
+import ruFacts from './ru.json'
+import enFacts from './en.json'
+import { IFact } from '@/types/facts.types'
+import { ELanguages } from '@/types/languages.types'
+
+const ru = ruFacts as IFact[]
+const en = enFacts as IFact[]
+
+export const facts: Record<`${ELanguages}`, IFact[]> = {
+  ru,
+  en
+}
