ede858eb6bea1fbd92d8aaba686c93b1e19dfbbd
paw.frolow <paw.frolow@gmail.com>
2024-03-08 21:52:53 +0300

    debug for dev env i18n

--- src/i18n.js
+++ src/i18n.js
@@ -21,7 +21,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
-    debug: true,
+    debug: process.env.NODE_ENV === 'development',
     defaultLocale: 'ru',
     locales: ['en', 'ru'],
 
